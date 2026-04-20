f505c3cc142447039c1ec4acf5231d6aa264f93d
Owen <scoutthemutt@proton.me>
2026-04-20 11:30:53 -0600

    test 6

--- Project_4/wall_follower.py
+++ Project_4/wall_follower.py
@@ -142,7 +142,7 @@ class WallFollower:
     def _execute(self, state):
         if state == 'FORWARD':
             # Negative = physical forward
-            self._robot.setWheelSpeedsRaw(0.38, 0.5)
+            self._robot.setWheelSpeedsRaw(-0.38, -0.5)
 
         elif state == 'STEER_AWAY':
             # Away from right wall = curve left: left faster forward (more negative)
