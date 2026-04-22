65efb65139bd46bc135be51fc3194cc576580daa
RyanBarrett <rybarrett11@gmail.com>
2026-04-22 12:33:03 -0600

    value test 1.9

--- Project_4/wall_follower.py
+++ Project_4/wall_follower.py
@@ -143,7 +143,7 @@ class WallFollower:
         # Left(Ch0): negative=forward. Right(Ch1): positive=forward.
         if state == 'FORWARD':
             print("[WALL-DEBUG] FORWARD")
-            self._robot.setWheelSpeedsRaw(1.2, -0.6)
+            self._robot.setWheelSpeedsRaw(-0.6, 0.6)
 
         elif state == 'STEER_AWAY':
             # Curve left: left backward, right forward fast
